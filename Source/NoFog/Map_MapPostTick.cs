0bd14b2356283fc09401d2e79aec7cc0bf2aa824
Mlie <emipa606@users.noreply.github.com>
2025-01-09 19:58:31 +0100

    Should now work with any pocket map, such as used in Ancient Urban Ruins

--- Source/NoFog/Map_MapPostTick.cs
+++ Source/NoFog/Map_MapPostTick.cs
@@ -9,11 +9,6 @@ public static class Map_MapPostTick
 {
     private static readonly HashSet<Map> doneMaps = [];
 
-    public static bool Prepare()
-    {
-        return NoFog.UsingStargate;
-    }
-
     public static void Postfix(Map __instance)
     {
         if (doneMaps.Contains(__instance))
--- Source/NoFog/NoFog.cs
+++ Source/NoFog/NoFog.cs
@@ -7,11 +7,8 @@ namespace NoFog;
 [StaticConstructorOnStartup]
 public static class NoFog
 {
-    public static bool UsingStargate;
-
     static NoFog()
     {
-        UsingStargate = ModsConfig.IsActive("CCYT.StargatesMod");
         new Harmony("Mlie.NoFog").PatchAll(Assembly.GetExecutingAssembly());
     }
 }
